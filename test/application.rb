f04046d68cecd4b4da0322c3b4e90cbcc2d6badc
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2012-07-08 14:12:44 +0200

    cleaned up test

--- test/application.rb
+++ test/application.rb
@@ -1,16 +1,6 @@
 require 'sinatra'
 require 'rinruby'
 
-error do
-  e = request.env['sinatra.error']
-  Kernel.puts e.backtrace.join("\n")
-  'Application error'
-end
-
-helpers do
-  # add your helpers here
-end
-
 # root page
 get '/' do
 
