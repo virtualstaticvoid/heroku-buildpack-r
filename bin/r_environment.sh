e28d1029ccdb186c8c126108820187af16993df1
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2013-12-10 03:34:02 +0200

    packages can find the GSL when compiling

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -5,3 +5,7 @@ GCC_VERSION="4.3"
 export R_HOME=/app/vendor/R
 export R_INCLUDE=$R_HOME/lib64/R/include
 export PATH=$R_HOME/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+
+# flags for compiling packages
+export PKG_LDFLAGS="-L/app/vendor/gsl/lib"
+export PKG_CPPFLAGS="-I/app/vendor/gsl/include"
