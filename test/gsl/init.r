8d4d208a9488e38efa0b7a75c2b485cb8c70e1b2
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2013-12-11 06:56:37 +0200

    included full set of packages for testing

--- test/gsl/init.r
+++ test/gsl/init.r
@@ -6,7 +6,7 @@
 ###########################################################
 # Update this line with the R packages to install:
 
-my_packages = c("topicmodels")
+my_packages = c("RJSONIO", "RPostgreSQL", "plyr", "tm", "XML", "Snowball", "slam", "topicmodels")
 
 ###########################################################
 
