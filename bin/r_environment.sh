93d12c3d95758094788751cacd219a48822dcfa7
Craig Schmidt <craig@craigschmidt.com>
2014-02-12 16:11:13 -0500

    change R_HOME to point to /app/vendor/R/lib64/R

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -2,6 +2,7 @@
 
 GCC_VERSION="4.3"
 
-export R_HOME=/app/vendor/R
-export R_INCLUDE=$R_HOME/lib64/R/include
-export PATH=$R_HOME/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+export R_BASE=/app/vendor/R
+export R_HOME=$R_BASE/lib64/R
+export R_INCLUDE=$R_HOME/include
+export PATH=$R_BASE/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
