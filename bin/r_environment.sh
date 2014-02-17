184bef0eef484364a2eb9d8ecf5817c38b07bf82
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-02-18 01:35:19 +0200

    trying to get topicmodels to compile

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -8,5 +8,7 @@ export R_INCLUDE=$R_HOME/include
 export PATH=$R_BASE/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
 
 # flags for compiling packages
-export PKG_LDFLAGS="-L/app/vendor/gsl/lib"
-export PKG_CPPFLAGS="-I/app/vendor/gsl/include"
+export LDFLAGS="-L/app/vendor/gcc/lib64 -L/app/vendor/gsl/lib"
+export CPPFLAGS="-I/app/vendor/glibc/string -I/app/vendor/glibc/time -I/app/vendor/gsl/include"
+export PKG_LDFLAGS=$LDFLAGS
+export PKG_CPPFLAGS=$CPPFLAGS
