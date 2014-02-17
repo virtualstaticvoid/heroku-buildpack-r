eee13f9f00351f7aa7429e0d5efddf040c179774
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-02-17 08:42:51 +0200

    add  PKG_LDFLAGS and PKG_CPPFLAGS to environment

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -6,3 +6,7 @@ export R_BASE=/app/vendor/R
 export R_HOME=$R_BASE/lib64/R
 export R_INCLUDE=$R_HOME/include
 export PATH=$R_BASE/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+
+# flags for compiling packages
+export PKG_LDFLAGS="-L/app/vendor/gsl/lib"
+export PKG_CPPFLAGS="-I/app/vendor/gsl/include"
