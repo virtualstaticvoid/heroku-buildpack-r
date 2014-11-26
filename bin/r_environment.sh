a9c3b9042703db998f84f249ae1dd330fbb974a2
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-11-27 01:12:36 +0200

    environment fixes

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export R_BASE="$vendordir/R"
-export R_INCLUDE="$R_BASE/lib64/R/include"
+export R_INCLUDE="/app/vendor/R/lib64/R/include"
 export PATH="/app/vendor/R/bin:/app/vendor/.apt/usr/bin:/app/bin:/usr/ruby1.9.2/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
 export LD_LIBRARY_PATH="/app/vendor/.apt/usr/lib/libblas:/app/vendor/.apt/usr/lib/lapack:/app/vendor/.apt/usr/lib/x86_64-linux-gnu:/app/vendor/.apt/usr/lib/i386-linux-gnu:/app/vendor/.apt/usr/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="/app/vendor/.apt/usr/lib/x86_64-linux-gnu:/app/vendor/.apt/usr/lib/i386-linux-gnu:/app/vendor/.apt/usr/lib:$LIBRARY_PATH"
