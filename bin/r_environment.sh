41f7f39d5bf4d1f1d9ecac4c5a0e092ffd45a78f
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-12-01 23:08:28 +0200

    correction to path environment variable

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export R_INCLUDE="/app/vendor/R/lib64/R/include"
-export PATH="/app/vendor/R/bin:/app/vendor/.apt/usr/bin:/app/bin:/usr/ruby1.9.2/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="/app/vendor/R/bin:/app/vendor/.apt/usr/bin:/app/bin:/usr/ruby1.9.2/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export LD_LIBRARY_PATH="/app/vendor/.apt/usr/lib/libblas:/app/vendor/.apt/usr/lib/lapack:/app/vendor/.apt/usr/lib/x86_64-linux-gnu:/app/vendor/.apt/usr/lib/i386-linux-gnu:/app/vendor/.apt/usr/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="/app/vendor/.apt/usr/lib/x86_64-linux-gnu:/app/vendor/.apt/usr/lib/i386-linux-gnu:/app/vendor/.apt/usr/lib:$LIBRARY_PATH"
 export INCLUDE_PATH="/app/vendor/.apt/usr/include:$INCLUDE_PATH"
