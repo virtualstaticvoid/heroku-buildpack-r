68ded71e313ec0873d1089475b55808b24035558
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2015-03-15 10:46:52 +0200

    added test app for executing package tests

---
+++ test/package-tests/init.r
@@ -0,0 +1,9 @@
+#
+# Example R code to install packages
+# See http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages for details
+#
+
+###########################################################
+# Update this line with the R packages to install:
+
+# file here for buildpack detection
