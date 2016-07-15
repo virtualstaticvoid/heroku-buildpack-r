531b68de0d9273a852de559f533e9e9287dbd30c
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-07-15 23:29:46 +0200

    improvements to buildpack compiile to support rgeos

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -6,8 +6,10 @@ GCC_VERSION=4.8
 
 export PATH="$HOME/vendor/R/bin:$HOME/.apt/usr/bin:$HOME/.apt/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
 export LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib:/usr/lib/x86_64-linux-gnu:/usr/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu/:$HOME/.apt/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION:/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION:$HOME/.apt/usr/lib/libblas:$HOME/.apt/usr/lib/lapack:$LD_LIBRARY_PATH"
-export LDFLAGS="-L$HOME/.apt/usr/lib/x86_64-linux-gnu -L$HOME/.apt/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION -L$HOME/.apt/usr/lib/libblas -L$HOME/.apt/usr/lib/lapack $LDFLAGS"
+export LD_LIBRARY_PATH="$HOME/.apt/usr/lib:$HOME/.apt/usr/lib/x86_64-linux-gnu/:$HOME/.apt/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION:/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION:$HOME/.apt/usr/lib/libblas:$HOME/.apt/usr/lib/lapack:$LD_LIBRARY_PATH"
+export LDFLAGS="-L$HOME/.apt/usr/lib -L$HOME/.apt/usr/lib/x86_64-linux-gnu -L$HOME/.apt/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION -L$HOME/.apt/usr/lib/libblas -L$HOME/.apt/usr/lib/lapack $LDFLAGS"
+export PKG_LIBS="$LDFLAGS $PKG_LIBS"
+export PKG_CPPFLAGS="-I$HOME/.apt/usr/include $PKG_CPPFLAGS"
 export PKG_CONFIG_PATH="$HOME/vendor/R/lib/pkgconfig:$HOME/.apt/usr/lib/x86_64-linux-gnu/pkgconfig:$HOME/.apt/usr/lib/pkgconfig:$PKG_CONFIG_PATH"
 export INCLUDE_PATH="$HOME/.apt/usr/include:$INCLUDE_PATH"
 export CPATH="$INCLUDE_PATH"
