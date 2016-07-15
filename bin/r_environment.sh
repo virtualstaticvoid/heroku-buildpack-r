66b69928016e70b1969b19a467bbd5697978e6fe
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-07-15 18:31:50 +0200

    fix location of binaries in slug so libblas is found

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -2,6 +2,8 @@
 
 GCC_VERSION=4.8
 
+# $HOME is /app
+
 export PATH="$HOME/vendor/R/bin:$HOME/.apt/usr/bin:$HOME/.apt/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
 export LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib:/usr/lib/x86_64-linux-gnu:/usr/lib:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu/:$HOME/.apt/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION:/usr/lib/gcc/x86_64-linux-gnu/$GCC_VERSION:$HOME/.apt/usr/lib/libblas:$HOME/.apt/usr/lib/lapack:$LD_LIBRARY_PATH"
