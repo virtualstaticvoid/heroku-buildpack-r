00d12a1954026952f9052754cc74b500067b058d
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-07-22 15:24:37 +0200

    improvements to capabilities example

--- test/capabilities/init.r
+++ test/capabilities/init.r
@@ -6,4 +6,16 @@
 ###########################################################
 # Update this line with the R packages to install:
 
-# file here for buildpack detection
+my_packages = c("nlme")
+
+###########################################################
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
