13aad8c71b5e3f401214f26b2fe5985dc50ebf5a
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-07-23 23:44:24 +0200

    updated topicmodels example

--- test/topicmodels/prog.r
+++ test/topicmodels/prog.r
@@ -2,4 +2,8 @@
 # Example R program
 #
 
-demo(is.things)
+library("topicmodels")
+
+data("AssociatedPress", package = "topicmodels")
+lda <- LDA(AssociatedPress[1:20,], control = list(alpha = 0.1), k = 2)
+lda_inf <- posterior(lda, AssociatedPress[21:30,])
