98bdb9a28a487560b3b6a3aa23954e0a6b07b389
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-07-24 20:09:29 +0200

    fixes rgeos and it's dependencies gdal and proj

--- test/rgeos/init.r
+++ test/rgeos/init.r
@@ -6,6 +6,19 @@
 ###########################################################
 # Update this line with the R packages to install:
 
+# gdal is tricky!
+# first, `before_compile` script hacks gdal-config to correct the directory locations
+# and then `proj` paths are overridden here via `configure.args`
+install.packages(
+  'rgdal',
+  type="source",
+  configure.args = c(
+    '--with-proj-include=/app/.apt/usr/local/include',
+    '--with-proj-lib=/app/.apt/usr/local/lib',
+    '--with-proj-share=/app/.apt/usr/share/proj'
+  )
+)
+
 my_packages = c("rgeos")
 
 ###########################################################
