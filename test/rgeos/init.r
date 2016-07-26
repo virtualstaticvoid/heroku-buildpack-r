22520a3190c167d54ee6b6fb0474d806424a3c8e
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-07-26 21:12:00 +0200

    install tweaks for automation of testing

--- test/rgeos/init.r
+++ test/rgeos/init.r
@@ -7,28 +7,18 @@
 # Update this line with the R packages to install:
 
 # gdal is tricky!
+
 # first, `before_compile` script hacks gdal-config to correct the directory locations
-# and then `proj` paths are overridden here via `configure.args`
+# and then `proj` paths are overridden here via `configure.args` to install.packages
+
 install.packages(
-  'rgdal',
+  "rgdal",
   type="source",
   configure.args = c(
-    '--with-proj-include=/app/.apt/usr/local/include',
-    '--with-proj-lib=/app/.apt/usr/local/lib',
-    '--with-proj-share=/app/.apt/usr/share/proj'
+    "--with-proj-lib=/app/.apt/usr/local/lib",
+    "--with-proj-include=/app/.apt/usr/local/include",
+    "--with-proj-share=/app/.apt/usr/share/proj"
   )
 )
 
-my_packages = c("rgeos")
-
-###########################################################
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
-  }
-  else {
-    cat(paste("Skipping already installed package:", p, "\n"))
-  }
-}
-invisible(sapply(my_packages, install_if_missing))
+install.packages("rgeos")
