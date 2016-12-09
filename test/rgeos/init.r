1f48217b2d0854057321081b594b483df6a99311
Chris Stefano <virtualstaticvoid@gmail.com>
2016-12-09 22:59:42 +0200

    rgeos test clean up

--- test/rgeos/init.r
+++ test/rgeos/init.r
@@ -6,7 +6,7 @@
 ###########################################################
 # Update this line with the R packages to install:
 
-my_packages = c("rgdal")
+my_packages = c("rgdal", "rgeos")
 
 ###########################################################
 
