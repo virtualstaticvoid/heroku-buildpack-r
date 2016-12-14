e308e3f9c41f56f05ed791992f40b12c4bf7bd3d
Chris Stefano <virtualstaticvoid@gmail.com>
2016-12-14 08:53:19 +0200

    ruby example working

--- test/ruby/application.rb
+++ test/ruby/application.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'fileutils'
+
+# prevent RinRuby from attempting to initialize, as
+# the R executable needs to be overriddden
+# see https://github.com/virtualstaticvoid/rinruby/blob/master/lib/rinruby.rb#L789
+R = :nil
+
 require 'rinruby'
+
+# initialize R interface, setting the "chroot" executable for R
+RInterface = RinRuby.new(:executable => "fakechroot fakeroot chroot /app/.root /usr/bin/R")
 
 # root page
 get '/' do
@@ -7,19 +17,19 @@ get '/' do
   sample_size = 10
 
   html = "<html>"
-  html += "<head><title>R Code Test</title></head>"
+  html += "<head><title>R Code Test From A Ruby Sinatra Web Application</title></head>"
   html += "<body>"
 
   html += "<p>Running R code...</p>"
 
   begin
 
-    R.eval "x <- rnorm(#{sample_size})"
-    R.eval "sdx <- sd(x)"
+    RInterface.eval "x <- rnorm(#{sample_size})"
+    RInterface.eval "sdx <- sd(x)"
 
     html += "<p>Succeeded running R code</p>"
-    html += "<pre>x = #{R.x}</pre>"
-    html += "<pre>sd(x) = #{R.sdx}</pre>"
+    html += "<pre>x = #{RInterface.x}</pre>"
+    html += "<pre>sd(x) = #{RInterface.sdx}</pre>"
     html += "<img src=\"plot.png\"></img>"
 
   rescue => e
@@ -32,16 +42,26 @@ get '/' do
 end
 
 get '/plot.png' do
+
   file = Tempfile.new('plot')
+  filename = File.basename(file.path)
+
+  # small issue here, since R is running within the chroot
+  # the file paths will be different, so a mapping between
+  # the host and the chroot is required, using a symlink
+  # into the chroot will work
+  FileUtils.ln_s(file.path, "/app/.root/app/#{filename}")
+
   code = <<-RCODE
-    png("#{file.path}", type="cairo")
+    png("/app/#{filename}", width=600, height=600)
     plot(1:5,1:5)
     dev.off()
   RCODE
-  R.eval(code)
+  RInterface.eval(code)
 
   send_file file.path, :type => :png
 
   file.close
   file.unlink
+
 end
