711ef3f6c347c82a666205030f9406ad55a157c1
Chris Stefano <virtualstaticvoid@gmail.com>
2016-12-18 16:48:19 +0200

    added more advanced shiny application

---
+++ test/shiny-adv/ui.R
@@ -0,0 +1,25 @@
+fluidPage(
+  # Application title
+  titlePanel("Word Cloud"),
+
+  sidebarLayout(
+    # Sidebar with a slider and selection inputs
+    sidebarPanel(
+      selectInput("selection", "Choose a book:",
+                  choices = books),
+      actionButton("update", "Change"),
+      hr(),
+      sliderInput("freq",
+                  "Minimum Frequency:",
+                  min = 1,  max = 50, value = 15),
+      sliderInput("max",
+                  "Maximum Number of Words:",
+                  min = 1,  max = 300,  value = 100)
+    ),
+
+    # Show Word Cloud
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
