8e1554f0f207f8331d6f685e23893b21e58050b5
Chris Stefano <virtualstaticvoid@gmail.com>
2016-12-18 17:11:09 +0200

    install missing packages for tests

--- test/package-tests/init.r
+++ test/package-tests/init.r
@@ -1,1 +1,24 @@
-# nothing doing
+#
+# Example R code to install packages
+# See http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages for details
+#
+
+###########################################################
+# Update this line with the R packages to install:
+
+my_packages = c(
+	"knitr",
+	"highlight"
+)
+
+###########################################################
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
