381ff0639e31e607fa3370ab225ef397d69d821b
Chris Stefano <virtualstaticvoid@gmail.com>
2017-01-12 09:23:52 +0200

    clean up JS code

--- test/nodejs/index.js
+++ test/nodejs/index.js
@@ -1,14 +1,17 @@
+// Example Node.js application to interface with R via RServe
 
-const spawn = require('child_process').spawn;
+// Need to create R in a child process since RServe is accessible
+// via a TCP port and Heroku only permits one port to be used
+// for use by the web process only.
+const Spawn = require('child_process').spawn;
 
-// fire up RServe within the same dyno as Node.js
-
-// const R = spawn('/usr/bin/R', ['-f', 'rserve.R', '--gui-none', '--no-save']); // use for local dev
-const R = spawn(
+// const R = Spawn('/usr/bin/R', ['-f', 'rserve.R', '--gui-none', '--no-save']); // use for local dev
+const R = Spawn(
   'fakechroot',
   ['fakeroot', 'chroot', '/app/.root', '/usr/bin/R', '-f', '/app/rserve.R', '--gui-none', '--no-save']
 );
 
+// hook up stdout and stderr for visibility into the child process
 R.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
 });
@@ -17,11 +20,10 @@ R.stderr.on('data', (data) => {
   console.log(`stderr: ${data}`);
 });
 
-var Rserve = require("rserve-js");
-var Express = require('express');
-var Promise = require('promise');
-
 // promise helper around rserve eval method
+
+const Promise = require('promise');
+
 function eval(client, rcode) {
   return new Promise(function(fulfill, reject) {
     client.eval(rcode, function(err, response) {
@@ -33,17 +35,22 @@ function eval(client, rcode) {
   });
 }
 
+const Express = require('express');
+const Rserve = require("rserve-js");
+
 var app = Express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
 
+	var rcode = "demo(is.things)";
+
   var client = Rserve.connect("localhost", 6311, function() {
 
     Promise.resolve(true)
       .then(function(_) {
-        return eval(client, "demo(is.things)");
+        return eval(client, rcode);
       })
       .then(function(result) {
         response.send("R Result\n" + result + "\n\n");
