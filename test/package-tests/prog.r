9ece1b1576b79ed8a05340f75e9931e5ea573955
Chris Stefano <virtualstaticvoid@gmail.com>
2017-02-08 22:49:42 +0200

    set working directory for package test outputs

--- test/package-tests/prog.r
+++ test/package-tests/prog.r
@@ -2,4 +2,9 @@
 # Run package tests
 #
 
+# run from the temp directory, as tests output a bunch of files...
+setwd("/var/tmp")
+
 sapply(installed.packages()[,"Package"], tools::testInstalledPackage)
+
+list.files()
