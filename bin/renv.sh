918a0c70fac6f3821437d0b63741fbba8c1fa3f2
Chris Stefano <virtualstaticvoid@gmail.com>
2017-05-30 23:33:57 +0200

    fix for runtime dependency on fakeroot

--- bin/renv.sh
+++ bin/renv.sh
@@ -3,5 +3,6 @@
 APP_DIR="/app"
 CHROOT_DIR="$APP_DIR/.root"
 TOOLS_DIR="$APP_DIR/.tools"
+FAKEROOT_DIR="$TOOLS_DIR/fakeroot"
 FAKECHROOT_DIR="$TOOLS_DIR/fakechroot"
-export PATH="$FAKECHROOT_DIR/sbin:$FAKECHROOT_DIR/bin:./bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
+export PATH="$FAKEROOT_DIR/bin:$FAKECHROOT_DIR/sbin:$FAKECHROOT_DIR/bin:./bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
