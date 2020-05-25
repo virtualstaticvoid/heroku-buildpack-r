4fc5668cad95e75a0a562ddb6e16e95c193f5d24
Chris Stefano <virtualstaticvoid@gmail.com>
2020-05-25 20:32:11 +0100

    updated since TinyTeX and pandoc included deploy image

--- bin/env.sh
+++ bin/env.sh
@@ -1,11 +1,14 @@
 # set path for testing within docker container
-export PATH="/app/R/lib/R/bin:/app/tcltk/bin:$PATH"
+export PATH="/app/R/lib/R/bin:/app/tcltk/bin:/app/TinyTeX/bin/x86_64-linux:/app/pandoc/bin:$PATH"
 
 # resolve R libs
 export LD_LIBRARY_PATH="/app/R/lib/R/lib:/app/tcltk/lib"
 
 # set user site library
 export R_LIBS_USER="/app/R/site-library"
+
+# path for pandoc
+export RSTUDIO_PANDOC=/app/pandoc/bin
 
 # default language
 export LANG=${LANG:-C.UTF-8}
